a1d715d0669ae3851ab47cb257ad10b098f310b1
Altay Kilic <altay.kilic@gmail.com>
2022-04-27 13:18:19 +0300

    simplified Move class

--- src/Move.hpp
+++ src/Move.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include "Pos.h"
 #include "Piece.h"
-#include "FEN.h"
 
 class Move{
 public:
 	int num;
-	FEN fen;
 	
 	Pos from;
 	Pos to;
@@ -20,18 +16,8 @@ public:
 	bool en_passant;
 	
 	//visual	
-	bool is_check;
-	//Nbd2
+	//Nbd2+
+	bool is_check;	// =1
 	bool show_file;	// =1
 	bool show_rank;	// =0
-	
-	std::vector<Move*> forward;
-	Move* backward;
-	
-	Move(){
-		backward = nullptr;
-	}
-	
-	void updateFEN(const std::string new_board);
-	std::string toString() const;
 };
